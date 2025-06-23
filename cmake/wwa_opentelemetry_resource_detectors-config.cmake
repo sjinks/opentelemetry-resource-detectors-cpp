95438e6562be18a2061bc8d907d5f580f9864e95
Volodymyr Kolesnykov <volodymyr@wildwolf.name>
2025-06-23 07:23:48 +0300

    fix: update cmake config

--- cmake/wwa_opentelemetry_resource_detectors-config.cmake
+++ cmake/wwa_opentelemetry_resource_detectors-config.cmake
@@ -3,7 +3,7 @@ get_filename_component(OTEL_RESOURCE_DETECTORS_CMAKE_DIR "${CMAKE_CURRENT_LIST_F
 list(APPEND CMAKE_MODULE_PATH ${OTEL_RESOURCE_DETECTORS_CMAKE_DIR})
 
 include(CMakeFindDependencyMacro)
-find_dependency(opentelemetry-cpp QUIET REQUIRED COMPONENTS resources)
+find_dependency(opentelemetry-cpp QUIET REQUIRED COMPONENTS sdk)
 
 if(NOT TARGET wwa_opentelemetry_resource_detectors)
     include("${OTEL_RESOURCE_DETECTORS_CMAKE_DIR}/wwa_opentelemetry_resource_detectors-target.cmake")
